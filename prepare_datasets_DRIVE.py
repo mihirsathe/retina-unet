dc7d7114020680a15fe2dce095249249a1631f5a
Mihir <m1sathe@ucsd.edu>
2018-09-07 13:22:11 -0700

    Python 3 conversion of high level scripts

--- prepare_datasets_DRIVE.py
+++ prepare_datasets_DRIVE.py
@@ -8,12 +8,9 @@ import os
 import h5py
 import numpy as np
 from PIL import Image
-
-
-def write_hdf5(arr, outfile):
-    with h5py.File(outfile, "w") as f:
-        f.create_dataset("image", data=arr, dtype=arr.dtype)
-
+import sys
+sys.path.insert(0, './lib/')
+from help_functions import write_hdf5
 
 #------------Path of the images --------------------------------------------------------------
 # train
@@ -25,19 +22,23 @@ original_imgs_test = "./DRIVE/test/images/"
 groundTruth_imgs_test = "./DRIVE/test/1st_manual/"
 borderMasks_imgs_test = "./DRIVE/test/mask/"
 #---------------------------------------------------------------------------------------------
-
-Nimgs = 20
+# Image Metadata
+n_imgs = 20 
 channels = 3
 height = 584
 width = 565
+
 dataset_path = "./DRIVE_datasets_training_testing/"
 
 
 def get_datasets(imgs_dir, groundTruth_dir, borderMasks_dir, train_test="null"):
-    imgs = np.empty((Nimgs, height, width, channels))
-    groundTruth = np.empty((Nimgs, height, width))
-    border_masks = np.empty((Nimgs, height, width))
-    # list all files, directories in the path
+
+    # Initialize arrays
+    imgs = np.empty((n_imgs, height, width, channels))
+    groundTruth = np.empty((n_imgs, height, width))
+    border_masks = np.empty((n_imgs, height, width))
+
+    # List all files, directories in the path
     for path, subdirs, files in os.walk(imgs_dir):
         for i in range(len(files)):
             # original
@@ -69,11 +70,11 @@ def get_datasets(imgs_dir, groundTruth_dir, borderMasks_dir, train_test="null"):
     print("ground truth and border masks are correctly withih pixel value range 0-255 (black-white)")
     # reshaping for my standard tensors
     imgs = np.transpose(imgs, (0, 3, 1, 2))
-    assert(imgs.shape == (Nimgs, channels, height, width))
-    groundTruth = np.reshape(groundTruth, (Nimgs, 1, height, width))
-    border_masks = np.reshape(border_masks, (Nimgs, 1, height, width))
-    assert(groundTruth.shape == (Nimgs, 1, height, width))
-    assert(border_masks.shape == (Nimgs, 1, height, width))
+    assert(imgs.shape == (n_imgs, channels, height, width))
+    groundTruth = np.reshape(groundTruth, (n_imgs, 1, height, width))
+    border_masks = np.reshape(border_masks, (n_imgs, 1, height, width))
+    assert(groundTruth.shape == (n_imgs, 1, height, width))
+    assert(border_masks.shape == (n_imgs, 1, height, width))
     return imgs, groundTruth, border_masks
 
 
